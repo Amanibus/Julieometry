e300aef226f636bd25d6159d30f85deaf3dfb350
Schwerdtner <schwerdt@math.tu-berlin.de>
2020-11-10 17:21:23 +0100

    Closes #2.

--- src/Shapes.jl
+++ src/Shapes.jl
@@ -1,5 +1,21 @@
+using LinearAlgebra
+
 abstract type AbstractShape end
 
 function area(sh::AbstractShape)
   throw("Not Implemented area")
+end
+
+struct Triangle{T} <: AbstractShape
+  p1::Vector{T}
+  p2::Vector{T}
+  p3::Vector{T}
+end
+
+function area(tr::Triangle)
+  b=p2-p1
+  l=norm(b)
+  bnorm=b/l
+  h=bnorm'*(p3-p1)
+  return abs(l*h/2)
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,1 +1,7 @@
-println("Initializing Tests....")
+import Pkg
+Pkg.add(["Test", "LinearAlgebra"])
+using Test, LinearAlgebra
+include("../src/Shapes.jl")
+
+tr=Triangle([0.0, 0.0], [0.0, 1.0], [1.0, 1.0])
+@test area(tf) ≈ 0.5
